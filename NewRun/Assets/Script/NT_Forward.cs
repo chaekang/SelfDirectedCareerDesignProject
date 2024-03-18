f92d156cdf056fcf27bba0fec191fabd4ba97767
chaekang <herry612@naver.com>
2024-03-18 15:48:57 +0900

    feat/신경전달물질 비율별로 이동

--- NewRun/Assets/Script/NT_Forward.cs
+++ NewRun/Assets/Script/NT_Forward.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class NT_Forward : MonoBehaviour
 {
     public float speed;
+
+    private void Start()
+    {
+        speed = Random.Range(15f, 25f);
+    }
+
     private void Update()
     {
         Vector2 position = transform.position;
         position.x -= -0.1f * speed * Time.deltaTime;
         transform.position = position;
+    }
+
+    private void Collision2D(Collider2D collision)
+    {
+        Debug.Log("Collision");
+        if (collision.CompareTag("NT_Destroy"))
+        {
+            GameManager.instance.pool.ReturnToPool(0, gameObject);
+        }
     }
 }
