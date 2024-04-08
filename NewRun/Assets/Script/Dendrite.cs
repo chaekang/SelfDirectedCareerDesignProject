216b82b5ff7dcf4244716c9fc95bacc87d6cb382
chaekang <herry612@naver.com>
2024-04-08 18:48:09 +0900

    pref/맵2 dendrite 전진 추가

--- NewRun/Assets/Script/Dendrite.cs
+++ NewRun/Assets/Script/Dendrite.cs
@@ -1,20 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Dendrite : MonoBehaviour
 {
     float time = 0;
+    bool appear = false;
+    public GameObject targetPosition;
+    float currentSpeed = 0f;
+    float acceleration = 2;
+
     void Update()
     {
         transform.localScale = new Vector3(0.07f, 0.07f, 1f) * (1 + time);
         time += Time.deltaTime;
 
-        if (time > 3.5f)
+        if (time > 3f)
         {
             transform.localScale = new Vector3(0.25f, 0.25f, 1f);
             GameManager.instance.player.appear = false;
+            appear = true;
         }
+        if (appear)
+        {
+            Vector3 direction = (targetPosition.transform.position - transform.position).normalized;
 
+            currentSpeed += acceleration * Time.deltaTime;
+
+            float distanceToMove = currentSpeed * Time.deltaTime;
+
+            transform.position += direction * distanceToMove;
+
+            if (Vector3.Distance(transform.position, targetPosition.transform.position) < 0.1f)
+            {
+                gameObject.SetActive(false);
+            }
+        }
     }
 }
