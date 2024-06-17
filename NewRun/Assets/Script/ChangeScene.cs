322606ba5ec0f29cd11aa9359370645c7d5b4540
SAILGO <121847777+SA-ILGO@users.noreply.github.com>
2024-06-17 22:40:38 +0900

    .
    .

--- NewRun/Assets/Script/ChangeScene.cs
+++ NewRun/Assets/Script/ChangeScene.cs
@@ -186,7 +186,7 @@ public class ChangeScene : MonoBehaviour
     public void start()
     {
         if (!isPoison)
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(14);
         else if (isPoison)
             SceneManager.LoadScene(6);
     }
