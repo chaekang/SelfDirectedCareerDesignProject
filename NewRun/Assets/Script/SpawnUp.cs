a9257c2c7f9da432b4abfbc7ed816a97faf8e967
chaekang <herry612@naver.com>
2024-06-18 16:40:08 +0900

    시냅스 거의 변경 완료

--- NewRun/Assets/Script/SpawnUp.cs
+++ NewRun/Assets/Script/SpawnUp.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnUp : MonoBehaviour
 {
     BoxCollider2D area;
+    public GameObject ntCling;
 
     private float timer;
 
@@ -12,19 +13,53 @@ public class SpawnUp : MonoBehaviour
     {
         area = GetComponent<BoxCollider2D>();
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (ntCling != null)
+        {
+            if (collision.gameObject.tag == "NT")
+            {
+                ntCling.SetActive(true);
+            }
+        }
+    }
+
     private void Update()
     {
-        if (GameManager.instance.spawner.ntFinish)
+        if (!GameManager.instance.player.onSynapse && GameManager.instance.player.disappear)
         {
-            Spawn();
+            ntCling.SetActive(false);
+        }
+
+        if (GameManager.instance.spawner.ntFinish && GameManager.instance.player.onSynapse)
+        {
+            timer += Time.deltaTime;
+
+            // curPoint에 따라 randomTime을 조정
+            float curPoint = GameManager.instance.SynapseBar.curPoint;
+
+            // curPoint가 0보다 작거나 같으면 분비되는 nt가 없도록 함
+            if (curPoint <= 0)
+            {
+                return;
+            }
+
+            float randomTime = Mathf.Lerp(0.2f, 1f, 1f - curPoint / 100f); // curPoint가 클수록 randomTime이 짧아짐
+
+            if (timer > randomTime)
+            {
+                Spawn();
+                timer = 0;
+            }
         }
     }
 
     private Vector2 GetRandomPosition(BoxCollider2D area)
     {
-        Vector2 basePosition = transform.position;  //오브젝트의 위치
+        Vector2 basePosition = transform.position;  // 오브젝트의 위치
 
-        //x, y축 좌표 얻기
+        // x, y축 랜덤 좌표 얻기
         float posX = basePosition.x;
         float posY = basePosition.y;
 
@@ -38,8 +73,15 @@ public class SpawnUp : MonoBehaviour
         float curPoint = GameManager.instance.SynapseBar.curPoint;
         Vector2 spawnPosition = GetRandomPosition(area);
 
-        GameObject spawnedPrefab = GameManager.instance.pool.Get(1);
-        spawnedPrefab.transform.position = spawnPosition;
-
+        if (GameManager.instance.changeScene.endStage)
+        {
+            GameObject spawnedPrefab = GameManager.instance.pool.Get(0);
+            spawnedPrefab.transform.position = spawnPosition;
+        }
+        else
+        {
+            GameObject spawnedPrefab = GameManager.instance.pool.Get(1);
+            spawnedPrefab.transform.position = spawnPosition;
+        }
     }
 }
