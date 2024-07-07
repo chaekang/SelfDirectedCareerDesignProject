554a881f404c023d50d699ae2514ae656a9afdb9
chaekang <herry612@naver.com>
2024-07-07 20:08:53 +0900

    초기화면 배경음악 및 인포창 수정

--- NewRun/Assets/Script/Info.cs
+++ NewRun/Assets/Script/Info.cs
@@ -8,49 +8,43 @@ public class Info : MonoBehaviour
 {
     public Image[] info;
     public GameObject info_pannel;
+    public GameObject exitBtn;
 
-    int index = 0; //sprite의 배열에대한 순서 첫번째 0부터시작
-    void Start()
-    {
-
-    }
-
-
-    void Update()
-    {
-
-    }
+    int index = 0; //sprite의 배열에 대한 순서 첫 번째 0부터 시작
 
     public void Click_info_btn()
     {
         info[index].gameObject.SetActive(true);
         info_pannel.SetActive(true);
+        exitBtn.SetActive(false); // info_pannel이 켜지면 exitBtn을 끔
     }
+
     public void Click_x_btn()
     {
         info[index].gameObject.SetActive(false);
         index = 0;
         info_pannel.SetActive(false);
+        exitBtn.SetActive(true); // info_pannel이 꺼지면 exitBtn을 켬
     }
+
     public void Click_rightbtn()
     {
-        Debug.Log("right click " + index);
-        if(info.Length > index+1)
+        if (info.Length > index + 1)
         {
             info[index].gameObject.SetActive(false);
             index++;
             info[index].gameObject.SetActive(true);
         }
-        else if (info.Length == index+1) 
+        else if (info.Length == index + 1)
         {
             info[index].gameObject.SetActive(false);
             index = 0;
             info[index].gameObject.SetActive(true);
         }
     }
+
     public void Click_leftbtn()
     {
-        Debug.Log("left click " + index);
         if (0 < index)
         {
             info[index].gameObject.SetActive(false);
@@ -60,7 +54,7 @@ public class Info : MonoBehaviour
         else if (index == 0)
         {
             info[index].gameObject.SetActive(false);
-            index = info.Length-1;
+            index = info.Length - 1;
             info[index].gameObject.SetActive(true);
         }
     }
